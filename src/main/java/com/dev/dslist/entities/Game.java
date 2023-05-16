094e5f3e985b9f6e9ac03a970d8d3547a0d2d98c
Emanuel Matias <matias.neto.edu@gmail.com>
2023-05-16 20:29:13 -0300

    Domain Model

--- src/main/java/com/dev/dslist/entities/Game.java
+++ src/main/java/com/dev/dslist/entities/Game.java
@@ -18,14 +18,17 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private Double score;
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
-    private String imgURL;
+    private Double score;
+    private String img_Url;
+
     @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
     @Column(columnDefinition = "TEXT")
     private String longDescription;
 
